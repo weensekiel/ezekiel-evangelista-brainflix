202688c93a2e7692c3f2a8c57cabe458028dde78
Ezekiel Evangelista <evangelista.ezekiel@hotmail.ca>
2024-03-17 12:31:56 -0400

    fix stytling for upload page tablet

--- src/pages/UploadForm/UploadForm.jsx
+++ src/pages/UploadForm/UploadForm.jsx
@@ -16,43 +16,48 @@ export function UploadForm() {
     <main className="upload-form">
       <hr />
       <h2>Upload Video</h2>
-      <p className="upload-form__title">VIDEO THUMBNAIL</p>
-      <img
-        src={thumbnail}
-        alt="video thumbnail"
-        className="upload-form__thumbnail"
-      />
-      <form action="">
-        <label htmlFor="videoTitle" className="upload-form__title">
-          TITLE YOUR VIDEO
-        </label>
-        <input
-          name="videoTitle"
-          type="text"
-          placeholder="Add a title to your video"
-          className="upload-form__input upload-form__input-title"
-        />
+      <div className="upload-form__container">
+        <div className="upload-form__thumbnail-container">
+          <p className="upload-form__title">VIDEO THUMBNAIL</p>
+          <img
+            src={thumbnail}
+            alt="video thumbnail"
+            className="upload-form__thumbnail"
+          />
+        </div>
+        <form className="upload-form__inputs">
+          <label htmlFor="videoTitle" className="upload-form__title--label">
+            TITLE YOUR VIDEO
+          </label>
+          <input
+            name="videoTitle"
+            type="text"
+            placeholder="Add a title to your video"
+            className="upload-form__input upload-form__input-title"
+          />
 
-        <label htmlFor="videoDesc" className="upload-form__title">
-          ADD A VIDEO DESCRIPTION
-        </label>
-        <input
-          name="videoDesc"
-          type="text"
-          placeholder="Add a description to your video"
-          className="upload-form__input upload-form__input-desc"
-        />
-      </form>
-
-      <button
-        onClick={clickHandler}
-        className="upload-form__button upload-form__button--upload"
-      >
-        PUBLISH
-      </button>
-      <button className="upload-form__button upload-form__button--cancel">
-        CANCEL
-      </button>
+          <label htmlFor="videoDesc" className="upload-form__title--label">
+            ADD A VIDEO DESCRIPTION
+          </label>
+          <input
+            name="videoDesc"
+            type="text"
+            placeholder="Add a description to your video"
+            className="upload-form__input upload-form__input-desc"
+          />
+        </form>
+      </div>
+      <div className="upload-form__buttons">
+        <button
+          onClick={clickHandler}
+          className="upload-form__button upload-form__button--upload"
+        >
+          PUBLISH
+        </button>
+        <button className="upload-form__button upload-form__button--cancel">
+          CANCEL
+        </button>
+      </div>
     </main>
   );
 }
